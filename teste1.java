400798d6625a83ebc3260d6989c2b0c9510da0af
Joaovitinn <132697125+Joaovitinn@users.noreply.github.com>
2024-09-04 21:32:28 -0300

    teste1.java

---
+++ teste1.java
@@ -0,0 +1,24 @@
+Testes feitos em java no eclipse
+
+TESTE 1
+public class Main {
+	public static void main(String[] args) {
+	
+   
+		int Indice = 13;
+		int soma = 0;
+		int k = 0;
+		
+		while(k < Indice ) {
+			k = k + 1;
+			soma = soma + k;
+		}
+		System.out.println(soma);
+			
+	
+	}
+}
+
+
+
+
